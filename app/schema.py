fd819345b94a1fde201b43e82a8358e49d0859d7
Kartik Dhawan <kartikdhawan@Kartiks-MacBook-Air.local>
2026-03-14 15:31:45 +0530

    First commit: Base setup of python & graphql

---
+++ app/schema.py
@@ -0,0 +1,29 @@
+import strawberry
+from typing import List
+
+
+@strawberry.type
+class User:
+    id: str
+    name: str
+
+
+def getUsersFn():
+    return [User(**row) for row in dummyUsers]
+
+
+def getUserByIdFn(root, id: str) -> User | None:
+    foundUser = [User(**row) for row in dummyUsers if row["id"] == id]
+    return foundUser[0] if len(foundUser) > 0 else None
+
+
+dummyUsers = [{"id": "1", "name": "Kartik"}, {"id": "2", "name": "KK"}]
+
+
+@strawberry.type
+class Query:
+    getUsers: List[User] = strawberry.field(resolver=getUsersFn)
+    getUserById: User | None = strawberry.field(resolver=getUserByIdFn)
+
+
+schema = strawberry.Schema(query=Query)
