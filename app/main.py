d6c0dfa06af475fe55a2a35934d2777d3f1eae6c
Kartik Dhawan <kartikdhawan@Kartiks-MacBook-Air.local>
2026-03-16 19:31:02 +0530

    004: Refactored graphql config setup

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
-from app.schema import schema
+from app.graphql.router import graphql_app
 
 app = FastAPI()
 
-graphql_app = GraphQLRouter(schema)
 
 app.include_router(graphql_app, prefix='/graphql')
 
